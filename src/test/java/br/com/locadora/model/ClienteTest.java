5ea5d87ae649a32bdc65b6e3fc4abc41c0965bc7
cassioveneza <cassioveneza@gmail.com>
2015-03-29 17:16:27 -0300

    Criados primeiros testes.

--- src/test/java/br/com/locadora/model/ClienteTest.java
+++ src/test/java/br/com/locadora/model/ClienteTest.java
@@ -1,18 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.locadora.model;
 
-import java.math.BigDecimal;
-import static org.testng.Assert.*;
-import org.testng.annotations.*;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
+import org.testng.annotations.Test;
 
 public class ClienteTest {
-
-    public ClienteTest() {
-    }
 
     @Test
     public void testCreateUpdate() {
@@ -29,7 +21,7 @@ public class ClienteTest {
         assertEquals(cliente.getNome(), nome);
         assertEquals(cliente.getTelefone(), telefone);
         assertEquals(cliente.getEndereco(), endereco);
-        
+
         final String nomeAlterado = "MARIA DA SILVA";
         Cliente clienteAlterado = Cliente.Builder.from(cliente)
                 .nome(nomeAlterado)
@@ -38,22 +30,6 @@ public class ClienteTest {
         assertEquals(cliente.getNome(), nomeAlterado);
         assertEquals(cliente.getTelefone(), telefone);
         assertEquals(cliente.getEndereco(), endereco);
-        
     }
 
-    @BeforeClass
-    public static void setUpClass() throws Exception {
-    }
-
-    @AfterClass
-    public static void tearDownClass() throws Exception {
-    }
-
-    @BeforeMethod
-    public void setUpMethod() throws Exception {
-    }
-
-    @AfterMethod
-    public void tearDownMethod() throws Exception {
-    }
 }
