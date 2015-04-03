feb8fb6928e7ad14e982880f57ed57b62741696b
cassioveneza <cassioveneza@gmail.com>
2015-04-03 10:44:54 -0300

    Testes de integração

--- src/test/java/br/com/locadora/resource/ClienteResourceIT.java
+++ src/test/java/br/com/locadora/resource/ClienteResourceIT.java
@@ -4,6 +4,7 @@ import br.com.locadora.dto.ClienteDto;
 import br.com.locadora.model.Cliente;
 import java.net.MalformedURLException;
 import java.net.URI;
+import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
@@ -20,6 +21,7 @@ import org.testng.annotations.Test;
 public class ClienteResourceIT {
 
     private static WebTarget target;
+    private static Client client;
 
     @Deployment(testable = false)
     public static WebArchive createDeployment() {
@@ -29,27 +31,56 @@ public class ClienteResourceIT {
 
     @BeforeClass
     public void before() throws MalformedURLException {
-        target = ClientBuilder.newClient().target(URI.create("http://localhost:8080/app/webresources/clientes"));
+        client = ClientBuilder.newClient();
+        target = client.target(URI.create("http://localhost:8080/app/webresources/clientes"));
     }
 
     @Test
     @RunAsClient
-    public void testCreate() throws Exception {
+    public void testMethods() throws Exception {
 
         final String nome = "JOAO SILVA";
         final String telefone = "9944-5588";
         final String endereco = "RUA GERAL";
 
+        //POST
         final ClienteDto clienteDto = ClienteDto.DtoBuilder.create()
                 .nome(nome)
                 .telefone(telefone)
                 .endereco(endereco)
                 .build();
 
-        final Entity<ClienteDto> entity = Entity.entity(clienteDto, MediaType.APPLICATION_JSON);
+        Response response = target.request().post(Entity.entity(clienteDto, MediaType.APPLICATION_JSON), Response.class);
+        Assert.assertEquals(response.getStatus(), Response.Status.CREATED.getStatusCode());
 
-        Response response = target.request().post(entity, Response.class);
-        Assert.assertEquals(response.getStatus(), 201);
+        Cliente clienteResponse = response.readEntity(Cliente.class);
+        Assert.assertNotNull(clienteResponse);
+
+        //GET ALL
+        response = target.request().get();
+        Assert.assertEquals(response.getStatus(), Response.Status.OK.getStatusCode());
+
+        //PUT
+        final String nomeAlterado = "JOAO DA SILVA E SILVA";
+        ClienteDto clienteAlterado = ClienteDto.DtoBuilder.from(clienteDto.toRepresentation(clienteResponse))
+                .nome(nomeAlterado)
+                .build();
+
+        target = client.target(target.getUriBuilder()).path("/" + clienteResponse.getId());
+        response = target.request().put(Entity.entity(clienteAlterado, MediaType.APPLICATION_JSON));
+        Assert.assertEquals(response.getStatus(), Response.Status.CREATED.getStatusCode());
+
+        Cliente clienteResponseUpdated = response.readEntity(Cliente.class);
+        Assert.assertNotNull(clienteResponseUpdated);
+        Assert.assertEquals(clienteResponseUpdated.getNome(), nomeAlterado);
+
+        //DELETE
+        response = target.request().delete();
+        Assert.assertEquals(response.getStatus(), Response.Status.NO_CONTENT.getStatusCode());
+
+        //GET
+        response = target.request().get();
+        Assert.assertEquals(response.getStatus(), Response.Status.NOT_FOUND.getStatusCode());
     }
 
 }
